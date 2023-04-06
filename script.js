5fc7c537ba48d505b9980c9d1d76d7b6e58b9a0d
Georgeches <georgechesire83@gmail.com>
2023-04-06 08:13:03 +0300

    create user interface and to function to handle POST requests

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.querySelector('.navbar').style.width = (screen.width + 50).toString()+'px'
+    let hero_section = document.querySelector('.hero-section')
+    let productsDiv = document.querySelector('.all-products')
+    let discountDiv = document.querySelector('.discounts')
+    hero_section.style.height = (screen.height - 180).toString()+'px'
+    hero_section.style.width = (screen.width + 50).toString()+'px'
+    productsDiv.style.width = (screen.width * 0.90).toString()+'px'
+    discountDiv.style.width = (screen.width * 0.90).toString()+'px'
+})
+
 let productsDiv = document.querySelector('.products')
 let productsNav = document.querySelector('.products-nav')
 let productsCategories = document.querySelectorAll('.category')
+
+function updateCart(obj){
+    fetch(`https://my-json-server.typicode.com/Georgeches/Ecommerce/cart`,{
+        method: 'POST',
+        headers: {
+            "Context-Type" : "application/json"
+        },
+        body: JSON.stringify(obj)
+    })
+    .then(res=>res.json())
+    .then(update=>console.log(update))
+}
+
+fetch(`https://my-json-server.typicode.com/Georgeches/Ecommerce/smartphones`)
+.then(res=>res.json())
+.then(data => displayProducts(data))
 
 for(let i = 0; i<productsCategories.length; i++){
     productsCategories[i].addEventListener('click', ()=>{
@@ -12,16 +39,19 @@ for(let i = 0; i<productsCategories.length; i++){
 }
 
 function displayProducts(arr){
+    while (productsDiv.firstChild) {
+        productsDiv.removeChild(productsDiv.firstChild);
+    }
     for(let obj of arr){ 
-        let product = document.createElement('li')
-        product.innerHTML = `
-            <img src="${obj.image}"alt="${obj.name}" height="200">
-            <div class="product-texts">
-                <p>${obj.name}</p>
-                <p>${obj.price}</p>
-                <button class="add-to-cart">Add to cart</button>
+        productsDiv.innerHTML += `
+            <div class="product">
+                <img src="${obj.image}"alt="${obj.name}" height="200">
+                <div class="product-texts">
+                    <p>${obj.name}</p>
+                    <p>${obj.price}</p>
+                    <button class="add-to-cart">Add to cart</button>
+                </div>
             </div>
         `
-        productsDiv.appendChild(product)
     }
 }
